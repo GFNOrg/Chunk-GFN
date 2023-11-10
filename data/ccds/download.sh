759bf82c2e5fbdf061684a1a7a0140068a2b9358
Joseph <joseph@viviano.ca>
2023-11-10 10:19:25 -0500

    data download

---
+++ data/ccds/download.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+wget -c ftp://ftp.ncbi.nih.gov/pub/CCDS/current_human/*
+
+#BuildInfo.20221027.txt
+#BuildInfo.current.txt
+#CCDS.20221027.txt
+#CCDS.current.txt
+#CCDS2Sequence.20221027.txt
+#CCDS2Sequence.current.txt
+#CCDS2UniProtKB.20221027.txt
+#CCDS2UniProtKB.current.txt
+#CCDS_attributes.20221027.txt
+#CCDS_attributes.current.txt
+#CCDS_exons.20221027.txt
+#CCDS_exons.current.txt
+#CCDS_nucleotide.20221027.fna.gz
+#CCDS_nucleotide.current.fna.gz
+#CCDS_protein.20221027.faa.gz
+#CCDS_protein.current.faa.gz
+#CCDS_protein_exons.20221027.faa.gz
+#CCDS_protein_exons.current.faa.gz
