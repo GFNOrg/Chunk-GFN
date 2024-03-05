5d32d0f1c27917a49d75a776144f330922e3b30b
jaggbow <oussama.boussif@umontreal.ca>
2024-03-05 11:44:46 -0500

    added rnn embedding and rnn unconditional models

--- chunkgfn/models/rnn.py
+++ chunkgfn/models/rnn.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -43,6 +44,106 @@ class RNN(nn.Module):
         out = self.logits_layer(out)
 
         return out
+
+
+class UnconditionalRNN(nn.Module):
+    def __init__(self, num_layers, hidden_dim, state_dim, act, n_actions):
+        super(UnconditionalRNN, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+        self.state_dim = state_dim
+        self.n_actions = n_actions
+
+        self.state_embedding = nn.Linear(state_dim, hidden_dim)
+
+        self.rnn_encoder = nn.GRU(
+            hidden_dim, hidden_dim, num_layers=2, batch_first=True
+        )
+
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(hidden_dim, hidden_dim))
+        self.layers.append(act)
+        for _ in range(self.num_layers):
+            self.layers.append(nn.Linear(hidden_dim, hidden_dim))
+            self.layers.append(act)
+        self.layers.append(nn.LayerNorm(hidden_dim))
+        self.logits_layer = nn.Linear(hidden_dim, n_actions)
+        self.logits_layer.weight.data.fill_(0.0)
+        self.logits_layer.bias.data.fill_(0.0)
+
+    def forward(self, state):
+        """
+        Args:
+            state [batch_size, max_len, state_dim]
+        """
+        s, _ = self.rnn_encoder(self.state_embedding(state))
+        out = s[:, -1]
+        for layer in self.layers:
+            out = layer(out)
+        out = self.logits_layer(out)
+
+        return out
+
+
+class RNN_Embedding(nn.Module):
+    def __init__(
+        self,
+        num_layers,
+        hidden_dim,
+        input_dim,
+        embedding_dim,
+        state_dim,
+        act,
+        n_actions,
+    ):
+        super(RNN_Embedding, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+        self.state_dim = state_dim
+        self.input_dim = input_dim
+        self.n_actions = n_actions
+        self.embedding_dim = embedding_dim
+
+        self.input_embedding = nn.Linear(input_dim, hidden_dim)
+        self.state_embedding = nn.Linear(state_dim, hidden_dim)
+        self.action_embeddings = nn.Parameter(torch.randn(n_actions, embedding_dim))
+
+        self.rnn_encoder = nn.GRU(
+            hidden_dim, hidden_dim, num_layers=2, batch_first=True
+        )
+
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(hidden_dim, hidden_dim))
+        self.layers.append(act)
+        for _ in range(self.num_layers):
+            self.layers.append(nn.Linear(hidden_dim, hidden_dim))
+            self.layers.append(act)
+        self.layers.append(nn.LayerNorm(hidden_dim))
+        self.emmbedding_layer = nn.Linear(hidden_dim, embedding_dim)
+        self.emmbedding_layer.weight.data.fill_(0.0)
+        self.emmbedding_layer.bias.data.fill_(0.0)
+
+    def get_logits(self, embedding):
+        logits = torch.matmul(embedding, self.action_embeddings.T)
+        logits = logits / (self.embedding_dim**0.5)  # Same scaling as in self attention
+        return logits
+
+    def forward(self, x, state):
+        """
+        Args:
+            x [batch_size, max_len, input_dim]
+            state [batch_size, max_len, state_dim]
+        """
+
+        _, x_ = self.rnn_encoder(self.input_embedding(x))
+        s, _ = self.rnn_encoder(self.state_embedding(state), x_)
+        out = s[:, -1]
+        for layer in self.layers:
+            out = layer(out)
+        embedding = self.emmbedding_layer(out)
+        logits = self.get_logits(embedding)
+
+        return logits
 
 
 class logZ(nn.Module):
