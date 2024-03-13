75d75b60e914fbcb5e80a6f6081c09fb9727b7e6
jaggbow <oussama.boussif@umontreal.ca>
2024-03-13 12:57:30 -0400

    edited sbatch scripts to take into account experiment configs

--- sbatch_scripts/bit_sequence/gfn_random_64.sh
+++ sbatch_scripts/bit_sequence/gfn_random_64.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#SBATCH --job-name=random-bs-64
 #SBATCH --partition=long
 #SBATCH --cpus-per-task=6
 #SBATCH --gres=gpu:rtx8000:1
@@ -12,4 +11,4 @@ module load libffi
 module load OpenSSL/1.1
 source $VENV/ai_scientist/bin/activate
 
-python main.py seed=42 data=bit_sequence gfn=tb_gfn_random trainer.max_epochs=1000 data.max_len=64 gfn.reward_temperature=0.3333 logger.wandb.name="random-len-64"
+python main.py seed=42 experiment=bit_sequence_random data.max_len=64 logger.wandb.name="random-len-64"
