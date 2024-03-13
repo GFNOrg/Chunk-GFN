096877d1df1c175cf76c4d137c57e8598412c98e
jaggbow <oussama.boussif@umontreal.ca>
2024-03-13 12:27:02 -0400

    added sbatch scripts for hypergrid

---
+++ sbatch_scripts/hypergrid/lib_0.01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=long
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --mem=48G
+#SBATCH --time=16:00:00
+#SBATCH -o /network/scratch/o/oussama.boussif/slurm-%j.out
+
+module --quiet load python/3.10
+module load libffi
+module load OpenSSL/1.1
+source $VENV/ai_scientist/bin/activate
+
+python main.py experiment=hypergrid_chunking data.R0=0.01 logger.wandb.name="lib-R0-0.01"
