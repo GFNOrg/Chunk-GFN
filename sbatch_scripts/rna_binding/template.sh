99e5e7549245ac8fe1a36bc4443e51a8a7ddc077
jaggbow <oussama.boussif@umontreal.ca>
2024-04-22 10:41:35 -0400

    added some scripts for rna binding

---
+++ sbatch_scripts/rna_binding/template.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=long
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --mem=48G
+#SBATCH --time=48:00:00
+#SBATCH -o /network/scratch/o/oussama.boussif/slurm-%j.out
+
+module --quiet load python/3.10
+module load libffi
+module load OpenSSL/1.1
+source $VENV/ai_scientist/bin/activate
+
+
+python main.py "$@"
