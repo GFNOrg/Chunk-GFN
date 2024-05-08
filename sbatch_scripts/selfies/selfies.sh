043373ee65f31300b17653f5408b3cfea31ce98f
jaggbow <oussama.boussif@umontreal.ca>
2024-05-08 10:19:18 -0400

    added sbatch scripts for various envs

--- sbatch_scripts/selfies/selfies.sh
+++ sbatch_scripts/selfies/selfies.sh
@@ -2,14 +2,12 @@
 #SBATCH --partition=long
 #SBATCH --cpus-per-task=3
 #SBATCH --gres=gpu:rtx8000:1
-#SBATCH --mem=18G
-#SBATCH --time=48:00:00
+#SBATCH --mem=24G
+#SBATCH --time=3-00:00:00
 #SBATCH -o /network/scratch/o/oussama.boussif/slurm-%j.out
 
 module --quiet load python/3.10
-module load libffi
-module load OpenSSL/1.1
 source $VENV/ai_scientist/bin/activate
 
 
-python main.py "$@"
+exec python main.py "$@"
