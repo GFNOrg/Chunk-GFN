6d16a63469f5e93760656d966f52772a9b8e8a1c
lena-nehale.ezzine <lena-nehale.ezzine@cn-b002.server.mila.quebec>
2024-05-14 16:00:43 -0400

    Fix the modes for bit sequence. Before, they were changing when we change the seed

--- sbatch_scripts/bit_sequence/bit_sequence.sh
+++ sbatch_scripts/bit_sequence/bit_sequence.sh
@@ -4,10 +4,10 @@
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --mem=24G
 #SBATCH --time=4-06:00:00
-#SBATCH -o /network/scratch/o/oussama.boussif/slurm-%j.out
+#SBATCH -o /home/mila/l/lena-nehale.ezzine/scratch/chunkgfn/slurm-%j.out
 
 module --quiet load python/3.10
-source $VENV/ai_scientist/bin/activate
+source /home/mila/l/lena-nehale.ezzine/venvs/chunk/bin/activate
 
 
 exec python main.py "$@"
