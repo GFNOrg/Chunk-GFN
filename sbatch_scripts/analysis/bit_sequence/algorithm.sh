9ab1234483fab168783d899850545f33525b9817
Joseph Viviano <joseph@viviano.ca>
2024-05-16 15:03:21 -0400

    bug

--- sbatch_scripts/analysis/bit_sequence/algorithm.sh
+++ sbatch_scripts/analysis/bit_sequence/algorithm.sh
@@ -27,20 +27,21 @@ do
             cutoff="${fields[1]}"
             cutoff=$((cutoff))
 
-            sbatch sbatch_scripts/analysis/bit_sequence.sh \
+            sbatch sbatch_scripts/analysis/bit_sequence/bit_sequence.sh \
             experiment=bit_sequence_chunk_prioritized \
             task_name=bit_sequence \
             seed=${seed} \
             data.max_len=${length} \
-            gfn.chunk_algorithm=bpe \
+            gfn.chunk_algorithm=${chunk_method} \
             gfn.library_update_frequency=10 \
             gfn.n_samples=10000 \
             gfn.replay_buffer.cutoff_distance=${cutoff} \
             gfn.reward_temperature=0.3333333333333333 \
             logger.wandb.name=bitseq_${chunk_method}_${length} \
             gfn.chunk_algorithm=${chunk_method} \
-            logger.wandb.group=bs_chunking_algorithm    
-            
+            logger.wandb.group=bs_chunking_algorithm
+            break
         done
     done
+    break
 done
