5c8e45fd26fee9cf4b82c71f57efa5ad5c6df66c
jaggbow <oussama.boussif@umontreal.ca>
2024-08-13 14:35:15 -0400

    changed uniform policy

--- chunkgfn/models/uniform_policy.py
+++ chunkgfn/models/uniform_policy.py
@@ -4,11 +4,9 @@ from .base_policy import BasePolicy
 
 
 class UniformPolicy(BasePolicy):
-    def __init__(self, action_embedding_dim: int):
+    def __init__(self, action_embedding_dim):
         super().__init__()
-        self.action_embedding_dim = action_embedding_dim
 
     def forward(self, state: torch.Tensor, *args, **kwargs) -> torch.Tensor:
-        return torch.zeros(
-            state.shape[0], self.action_embedding_dim, device=state.device
-        )
+        env = self._environment
+        return torch.zeros(state.shape[0], env.n_actions, device=state.device)
