b8d6fb20ec0a814c43c3462605298de8a8535bf6
jaggbow <oussama.boussif@umontreal.ca>
2024-08-27 08:35:52 -0400

    added MLP critic

--- chunkgfn/models/mlp.py
+++ chunkgfn/models/mlp.py
@@ -39,3 +39,36 @@ class MLP(BasePolicy):
         action_embedding = self.action_embedding_layer(out)
 
         return action_embedding
+
+
+class Critic(BasePolicy):
+    def __init__(self, num_layers, hidden_dim, in_dim, activation):
+        super(Critic, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+        self.in_dim = in_dim
+
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(in_dim, hidden_dim))
+        self.layers.append(activation)
+        for _ in range(self.num_layers):
+            self.layers.append(nn.Linear(hidden_dim, hidden_dim))
+            self.layers.append(activation)
+        self.layers.append(nn.LayerNorm(hidden_dim))
+        # This layer generates the action embedding that will be used for picking the next action
+        self.action_embedding_layer = nn.Linear(hidden_dim, 1)
+
+    def forward(self, state):
+        """
+        Generate the action embedding given a state
+        Args:
+            state [batch_size, dim]: Input state.
+        Returns:
+            action_embedding [batch_size, action_embedding]: Action embedding.
+        """
+        out = state
+        for layer in self.layers:
+            out = layer(out)
+        action_embedding = self.action_embedding_layer(out)
+
+        return action_embedding
